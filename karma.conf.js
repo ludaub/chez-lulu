22b2ff93c7fdac90b161dbf972e3edcbdac0fe65
ludaub <ludovic.aubriere@gmail.com>
2021-10-01 23:36:02 +0200

    chore: rename app title

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/lulu-s'),
+      dir: require('path').join(__dirname, './coverage/lulu-s-bar'),
       subdir: '.',
       reporters: [
         { type: 'html' },
