079a7c745d21736b674652a22407e324a96c2441
ludaub <ludovic.aubriere@gmail.com>
2021-11-17 18:21:56 +0100

    test: init theme service tests

---
+++ src/app/core/theme.service.spec.ts
@@ -0,0 +1,23 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { ThemeService } from '@app/core/theme.service';
+
+describe('ThemeService', () => {
+  let service: ThemeService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ThemeService);
+  });
+
+  it('should create the service', () => {
+    expect(service).toBeTruthy();
+  });
+});
